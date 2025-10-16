e2ef182aa28c3f787cdb6ebbf95578d05f6c3cdd
Doulor <adoulor@outlook.com>
2025-10-16 16:30:53 +0800

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,5 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY as string;
 
-// 初始化客户端
+// 初始化客户端aa
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
