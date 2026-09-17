401b504b398d21002b8ed877529a9fc8e83a19f1
Doulor <2737855297@qq.com>
2026-09-17 19:56:09 +0800

    .

--- src/config.ts
+++ src/config.ts
@@ -258,7 +258,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Doulor",
-	bio: "世界很大，不妨到处走走",
+	bio: "旧人不知我近况,新人不知我过往",
 	links: [
 		{
 			name: "Home",
--- src/content/thoughts/ytx来我家玩了.md
+++ src/content/thoughts/ytx来我家玩了.md
@@ -1,5 +1,5 @@
 ---
 content: "ytx来我家玩了"
 date: "2025-10-27"
-context: "傍晚的随想"
+context: "开学前"
 ---
